d6e51c1d61d50a7c4497b301c0d9db76560870b4
lyg509 <dydrjs252@naver.com>
2024-08-21 12:21:54 +0900

    layout.tsx 추가

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { Recursive } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const recursive = Recursive({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={recursive.className}>
+        <Navbar />
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full"></div>
+        </main>
+      </body>
+    </html>
+  );
+}
